b957949599976dc6f7b015cbc99c1216cbee7663
Marco Lüthy <marco@luethy.net>
2017-07-26 09:59:04 +0200

    some clean up

--- serverless/src/session.ts
+++ serverless/src/session.ts
@@ -1,4 +1,3 @@
-//import { LocalChrome, Queue, ChromelessOptions } from 'chromeless'
 import { connect as mqtt, MqttClient } from 'mqtt'
 import * as cuid from 'cuid'
 import { createPresignedURL } from './utils'
@@ -8,24 +7,11 @@ const debug = require('debug')('session handler')
 export default async (event, context, callback): Promise<void> => {
   debug('function invoked with event data: ', event)
 
+  const url = createPresignedURL()
   const channelId = cuid()
-
-  const TOPIC_CONNECTED = `chrome/${channelId}/connected`
-  const TOPIC_REQUEST = `chrome/${channelId}/request`
-  const TOPIC_RESPONSE = `chrome/${channelId}/response`
-  const TOPIC_END = `chrome/${channelId}/end`
-
-  /*
-  const lambda = new Lambda()
-
-  await lambda.invoke({
-    FunctionName: getFunctionName(remoteOptions),
-    Payload,
-  }).promise()
-  */
 
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ url: createPresignedURL(), channelId }),
+    body: JSON.stringify({ url, channelId }),
   })
 }
