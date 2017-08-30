d682724125bdfd411e3f8e09c7dbe1b6983e1b78
Teresa Ritorto <tritorto@gmail.com>
2017-08-30 10:20:47 +1000

    added warmup logic to session lambda

--- serverless/src/session.ts
+++ serverless/src/session.ts
@@ -6,6 +6,13 @@ export default async (event, context, callback): Promise<void> => {
   const url = createPresignedURL()
   const channelId = cuid()
 
+  // Warmup check
+  console.log(JSON.stringify(event))
+  if (event && event.warmup) {
+    console.log('Invoking Lambda for Warmup')
+    return callback(null, 'Lambda is warm')
+  }
+
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({ url, channelId }),
