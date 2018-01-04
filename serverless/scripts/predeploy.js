2a2df31639af39631976343afbc787e70de8f241
Marco Lüthy <marco@luethy.net>
2018-01-03 19:01:18 -0800

    feat(proxy-service): don't hardcode AWS_REGION
    Removes the hardcoded AWS_REGION from the Serverless config and adds a guard to the predeploy script to check that AWS_REGION is set.

--- serverless/scripts/predeploy.js
+++ serverless/scripts/predeploy.js
@@ -1,11 +1,37 @@
+if (typeof process.env.AWS_REGION === 'undefined') {
+  console.error(`
+Please set the "AWS_REGION" environment variable to the AWS region to which you wish to deploy.
+
+These are some cheap options:
+
+Ireland:        eu-west-1
+USA, Virginia:  us-east-1
+USA, Oregon:    us-west-2
+
+For example:
+
+export AWS_REGION=eu-west-1
+`)
+  process.exit(1)
+}
+
 if (typeof process.env.AWS_IOT_HOST === 'undefined') {
   console.error(`
-Please set the "AWS_IOT_HOST" environment variable to your region's AWS IoT Custom Endpoint.
+Please set the "AWS_IOT_HOST" environment variable to your ${
+    process.env.AWS_REGION
+  } region's AWS IoT Custom Endpoint.
 
 You can find it here:
-  https://eu-west-1.console.aws.amazon.com/iot/home#/settings
+  https://eu-west-1.console.aws.amazon.com/iot/home?region=${
+    process.env.AWS_REGION
+  }#/settings
 Or with the AWS CLI:
-  aws iot describe-endpoint --output text
+  aws iot describe-endpoint --output text --region ${process.env.AWS_REGION}
+
+For Example:
+
+export AWS_REGION=replacethispart.iot.eu-west-1.amazonaws.com
+
 `)
   process.exit(1)
 }
